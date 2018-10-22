7a71e24f0df1a473a9053fb1f27e86e977d0f6f5
abdul ahmad <ahmadabdul3@yahoo.com>
2018-10-22 11:32:59 -0400

    Make sequelize config read from the right node environment

--- db/models/index.js
+++ db/models/index.js
@@ -16,8 +16,8 @@ models.Sequelize = Sequelize;
 export default models;
 
 function initializeDatabase() {
-  const { database, username, password } = config[process.env];
-  return new Sequelize(database, username, password, config[process.env]);
+  const { database, username, password } = config[process.env.NODE_ENV];
+  return new Sequelize(database, username, password, config[process.env.NODE_ENV]);
 }
 
 function initializeModels() {
