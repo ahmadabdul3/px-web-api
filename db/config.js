0191ebc781cadeb1e99d21c74ce304295b6e8eb0
abdul ahmad <ahmadabdul3@yahoo.com>
2018-10-22 12:04:31 -0400

    Take out shared db shared config and reuse

--- db/config.js
+++ db/config.js
@@ -2,27 +2,21 @@
 //   correctly see the env variables
 const dotenv = require('dotenv').config();
 
-module.exports = {
-  production: {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    define: {
-      underscored: false
-    },
+const shared = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  define: {
+    underscored: false
   },
-  development: {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    define: {
-      underscored: false
-    },
-  },
-}
+};
+
+const config = {
+  production: shared,
+  development: shared,
+};
+
+export default config;
